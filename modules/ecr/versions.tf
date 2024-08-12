3204965c94c0029185c1172c31c0f8329f1481e1
sayafayas <akimratin@gmail.com>
2024-08-12 09:50:15 +0300

    change in module versions

--- modules/ecr/versions.tf
+++ modules/ecr/versions.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = ">= 1.0.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.6"
+      version = "~> 5.0"
     }
   }
 }
