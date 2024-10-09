371ffbd71dc734b47928237e71547189837aa2bc
Akim Ratin <akimratin@gmail.com>
2024-10-09 12:53:41 +0300

    Add output for control_panel

--- modules/networking/sg/outputs.tf
+++ modules/networking/sg/outputs.tf
@@ -2,3 +2,8 @@ output "all_worker_node_groups_sg_id" {
   description = "K8s Nodes SG"
   value       = aws_security_group.all_worker_node_groups.id
 }
+
+output "eks_control_plane_sg_id" {
+  description = "Control Panel SG"
+  value       = aws_security_group.eks_control_plane_sg_id
+}
