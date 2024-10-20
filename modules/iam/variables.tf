51f915bbc41dcc1b8ac062e514abb92ac7b4a043
Akim Ratin <akimratin@gmail.com>
2024-10-20 11:13:52 +0300

    delete not used iam module

--- modules/iam/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "role_name" {
-  description = "The name of the IAM role"
-  type        = string
-}
-
-variable "sso_role_arn" {
-  description = "The ARN of the SSO role that can assume this role"
-  type        = string
-}
